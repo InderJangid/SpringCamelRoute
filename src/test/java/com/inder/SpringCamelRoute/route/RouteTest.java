ca0e81a205b0eac7a55e75c4abf004ac4d450a90
jrawatRepo <jrawatmay89@gmail.com>
2020-07-16 00:02:41 +0530

    Updated consumer template to test processing.

--- src/test/java/com/inder/SpringCamelRoute/route/RouteTest.java
+++ src/test/java/com/inder/SpringCamelRoute/route/RouteTest.java
@@ -1,5 +1,7 @@
 package com.inder.SpringCamelRoute.route;
 
+import org.apache.camel.ConsumerTemplate;
+import org.apache.camel.Exchange;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.test.junit4.CamelTestSupport;
@@ -7,6 +9,7 @@ import org.apache.camel.test.spring.CamelSpringRunner;
 import org.apache.camel.test.spring.DisableJmx;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -21,7 +24,10 @@ import org.springframework.test.context.ActiveProfiles;
 class RouteTest extends CamelTestSupport {
 
 	@Produce(uri = "jms:queue:testinput")
-	protected ProducerTemplate start2;
+	protected ProducerTemplate producer;
+	
+	@Autowired
+	protected ConsumerTemplate consumer;
 
 	/*
 	 * @EndpointInject(uri = "jms:queue:testoutput") protected MockEndpoint mockB;
@@ -30,7 +36,10 @@ class RouteTest extends CamelTestSupport {
 	@Test
 	public void testMocksAreValid() throws Exception {
 		//mockB.expectedMessageCount(1);
-		start2.sendBody("jagga");
+		//https://livebook.manning.com/book/camel-in-action/appendix-c/34
+		producer.sendBody("jagga");
+		Exchange response = consumer.receive("jms:queue:testoutput", 30000);
+		System.out.println("response "+ response.getIn().getBody(String.class));
 
 	}
 }
